0729116c5d2b82f2670ba161a6071df2d8cbae4d
saran <sarandeep1999@gmail.com>
2023-09-08 21:02:39 +0200

    add comments frontend.py

--- frontend.py
+++ frontend.py
@@ -36,6 +36,7 @@ st.write(
     """
 )
 
+# Initialization
 if "iteration" not in st.session_state:
     st.session_state.iteration = 1
 
@@ -69,6 +70,7 @@ def train_model_style(epoch_value):
 uploaded_file = st.file_uploader("Choose a dataset (CSV)", type="csv")
 
 if uploaded_file:
+    # This button is responsible for annotating the dataset using GPT 3.5
     if st.button("Annotate"):
         with st.spinner('Annotating rows using GPT 3.5...'):
             files = {'file': uploaded_file.getvalue()}
@@ -95,6 +97,7 @@ if uploaded_file:
             st.error("Failed to annotate dataset.")
 
 if "filtered_dataset" in st.session_state:
+    # Shows the rows with low confidence values
     st.dataframe(st.session_state.filtered_dataset, use_container_width=True)
 
     row_options = list(st.session_state.filtered_dataset.index)
@@ -104,6 +107,7 @@ if "filtered_dataset" in st.session_state:
 
     col1, col2, col3 = st.columns([1, 2, 1])
     with col1:
+        # Human annotating the GPT 3.5 annotated dataset
         if st.button("Update Label"):
             st.session_state.filtered_dataset.loc[row_selection, "predicted_labels"] = new_label
 
@@ -126,7 +130,7 @@ if "filtered_dataset" in st.session_state:
         """,
         unsafe_allow_html=True
     )
-
+    # Merge the human annotated low confidence rows with GPT 3.5 Annotated Dataset
     if st.button("Merge and Save", key="customSaveButton"):
         try:
             if 'dataset' not in st.session_state:
@@ -139,7 +143,8 @@ if "filtered_dataset" in st.session_state:
                 merged_dataset['predicted_labels_x'])
 
             merged_dataset = merged_dataset.drop(columns=['predicted_labels_x', 'predicted_labels_y'])
-
+            # get_next_version provides for rudimentary data versioning
+            # The GPT 3.5 + Human annotated dataset ( merged dataset ) is saved under data/merged
             version = get_next_version("data/merged", 'merged_')
             save_path = Path("data/merged") / f"merged_{version}.csv"
             merged_dataset.reset_index(drop=True)
@@ -148,28 +153,29 @@ if "filtered_dataset" in st.session_state:
 
             st.success(f"Dataset saved successfully at {save_path}")
             st.session_state.merged_successful = True
-            st.write('Allocating 20% of the rows as a hold-out test set')
+            # To get more consistent test results across models, we use a static test set
+            st.success('Allocating 20% of the rows as a hold-out test set')
 
             train_data, test_data = train_test_split(merged_dataset, test_size=0.2)
-            print(type(train_data))
-            print(train_data.head())
+
+            # Train data is saved under data/trainsets
             train_version = get_next_version("data/trainsets", 'train_')
             train_save_path = Path("data/trainsets") / f"train_{train_version}.csv"
             train_data.reset_index(drop=True)
             train_data.to_csv(train_save_path, index=False)
 
+            # Test data is saved under data/testsets
             test_version = get_next_version("data/testsets", 'test_')
             test_save_path = Path("data/testsets") / f"test_{test_version}.csv"
             test_data.reset_index(drop=True)
             test_data.to_csv(test_save_path, index=False)
-
+            # Logging these to st.session_state for later use
             st.session_state.test_set_path = test_save_path
             st.session_state.train_save_path = train_save_path
-            st.success('Train and Test sets saved under data/trainsets and data/testsets respectively')
 
         except Exception as e:
             st.error(f"An error occurred: {e}")
-
+    # After the GPT 3.5 + Human annotated dataset has been saved, train BERT model on it
     if st.session_state.get('merged_successful'):
         st.write("----")
         st.session_state.experiment_name = st.text_input("Enter the experiment name:",
@@ -177,19 +183,22 @@ if "filtered_dataset" in st.session_state:
 
         epoch_input = int(st.text_input("Enter the number of Epochs for BERT Training:", value="1"))
         model_name_inp = st.text_input("Enter the model name:", value="bert_sentiment_gpt35")
-
+        # Training BERT on the GPT 3.5 + Human annotated dataset for n epochs
         if st.button("Train Model"):
             training_message = train_model_style(epoch_input)
             st.markdown(training_message, unsafe_allow_html=True)
             progress_bar = st.empty()
 
+            # This method is responsible for showing the model training progress bar
             def update_progress(current_epoch, total_epochs):
                 progress = current_epoch / total_epochs
-                progress_bar.progress(progress)  # Update the progress bar in its slot
+                progress_bar.progress(progress)
+
 
             if not hasattr(st.session_state, 'save_path'):
                 st.warning("No dataset available for training. Please upload, annotate, and then merge first.")
             else:
+                # Training BERT
                 model_path, val_acc, model = train_bert(model_path=f"models/{model_name_inp}.pt",
                                                         train_data_path=st.session_state.train_save_path,
                                                         test_data_path=st.session_state.test_set_path,
@@ -207,18 +216,22 @@ if "filtered_dataset" in st.session_state:
 
     if st.session_state.get('stop_iterations', False):
         st.stop()
-
+    # After the initial BERT Training on the GPT 3.5 + Human annotated dataset, we use CleanLab to find label issues
+    # This is done in an iterative manner to enhance the quality of the final dataset and the model test results
     if st.session_state.get('initial_training') and not getattr(st.session_state, 'stop_iterations', False):
         heading_style = cleanlab_style()
         st.markdown(heading_style, unsafe_allow_html=True)
         st.write("----")
         st.subheader(f"Iteration: {st.session_state.iteration}")
+        # If it's the first iteration, we take the BERT model trained on the initial Human + GPT dataset
         if st.session_state.iteration == 1 and st.session_state.initial_training:
             st.session_state.current_model = st.session_state.initial_model
             st.session_state.current_data_path = st.session_state.train_save_path
             print('Iteration 1 data_path:', st.session_state.current_data_path)
 
+        # Else we choose the previous iteration's dataset and model
         else:
+            #
             model_path = f"models/model_cleanlab_{st.session_state.iteration - 1}.pt"
             loaded_model = torch.load(model_path)
             st.session_state.current_model = loaded_model
@@ -234,9 +247,11 @@ if "filtered_dataset" in st.session_state:
             st.session_state.top20_status = True
 
         if st.session_state.display_top_20:
+            # If the CleanLab process was a success, we display 20 rows with the lowest label scores
             st.dataframe(st.session_state.top_20, use_container_width=True)
 
         if st.session_state.top20_status:
+            # Providing an interface for the human to annotate these rows with label issues
             st.subheader("Label Issues for Annotation")
             row_options = list(st.session_state.top_20.index)
             row_selection = st.selectbox("Edit label for row:", options=row_options, key="row_selection")
@@ -249,7 +264,7 @@ if "filtered_dataset" in st.session_state:
                     st.session_state.top_20.loc[row_selection, 'predicted_labels'] = new_label
                     st.session_state.display_top_20 = True
 
-            # Button to merge and save cleaned data
+            # We finally merge these annotated rows with the dataset
             if st.button("Merge and Save Cleaned Data", key="merge_clean"):
                 original_data = pd.read_csv(st.session_state.current_data_path)
 
@@ -261,13 +276,14 @@ if "filtered_dataset" in st.session_state:
 
                 merged_dataset = merged_dataset.drop(columns=['predicted_labels_x', 'predicted_labels_y'])
 
-                # Save the merged dataset
+                # The cleaned dataset is saved under data/cleaned/cleaned_i , where i denotes the iteration number
                 save_cleaned_path = f"data/cleaned/cleaned_{st.session_state.iteration}.csv"
                 merged_dataset.to_csv(save_cleaned_path, index=False)
                 st.success(f"Cleaned data saved at: {save_cleaned_path}")
                 st.session_state.save_cleaned_path = save_cleaned_path
                 setattr(st.session_state, f'data_cleaning_{st.session_state.iteration}', True)
 
+        # Once the cleaned dataset has been saved, we train BERT on it and get evaluation metrics
         if getattr(st.session_state, f'data_cleaning_{st.session_state.iteration}', False):
             st.write("----")
             epoch_input = int(
@@ -279,13 +295,16 @@ if "filtered_dataset" in st.session_state:
 
                 progress_bar = st.empty()
 
+                # This method is responsible for showing the model training progress bar
                 def update_progress(current_epoch, total_epochs):
                     progress = current_epoch / total_epochs
-                    progress_bar.progress(progress)  # Update the progress bar in its slot
+                    progress_bar.progress(progress)
+
 
                 if not hasattr(st.session_state, 'save_cleaned_path'):
                     st.warning("No dataset available for training. Please upload, annotate, and then merge first.")
                 else:
+                    # Training BERT on cleaned dataset for iteration i
                     model_path, val_acc, model = train_bert(
                         model_path=f"models/model_cleanlab_{st.session_state.iteration}.pt",
                         train_data_path=st.session_state.save_cleaned_path,
@@ -301,13 +320,15 @@ if "filtered_dataset" in st.session_state:
                     st.session_state.bert_clean_training = True
                     setattr(st.session_state, f'iteration_{st.session_state.iteration}', True)
 
+        # The iteration number is increased if data cleaning for previous iteration is completed
+        # and if the entire iteration has processed
         if (getattr(st.session_state, f'iteration_{st.session_state.iteration}', False)
                 and getattr(st.session_state, f'data_cleaning_{st.session_state.iteration}', False)):
             st.session_state.iteration += 1
             st.session_state.top20_status = False
             setattr(st.session_state, f'iteration_{st.session_state.iteration}', False)
 
-            col_next, col_stop = st.columns(2)  # Creating two columns
+            col_next, col_stop = st.columns(2)
 
             with col_next:
                 if st.button("Next Iteration"):
